b889043f8e0cc1a3a064fb8214c8bb822d45dc26
Freddy Jimenez <frejime@amazon.com>
2024-04-01 17:52:06 -0500

    updated awsmobile

--- src/index.js
+++ src/index.js
@@ -4,11 +4,11 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Amplify } from 'aws-amplify'
-import config from './aws-exports'
+import awsmobile from './aws-exports'
 import '@aws-amplify/ui-react/styles.css'
 import { ThemeProvider } from '@aws-amplify/ui-react'
 
-Amplify.configure(config)
+Amplify.configure(awsmobile)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
